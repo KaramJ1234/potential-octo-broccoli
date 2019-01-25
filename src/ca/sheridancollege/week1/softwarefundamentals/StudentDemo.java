fac8c9734c3e96b3863d7185ce74b607aa823952
burning-cinnamon <41842208+burning-cinnamon@users.noreply.github.com>
2019-01-25 17:20:47 -0500

    1st commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -13,6 +13,7 @@ public class StudentDemo
 {
   private String studentID;
   private String name;
+  private String program;
   
   /**
    * A constructor to pass in the given ID and then name
@@ -49,7 +50,13 @@ public class StudentDemo
     public String getName() {
         return name;
     }
+    public String getProgram() {
+        return program;
 
+}
+public void setProgram(String program) {
+    this.program = program;
+}
     /**
      * A method to set the student's name
      * @param givenName the name to set
